42de81b3778fcc5903b5b4fbf8834d2590ac851f
Rithviik Srinivasan <rithu6876@gmail.com>
2023-11-26 14:05:42 -0500

    Fixed Door input for player

--- Nightmare_Descent_Into_Darkness/Assets/Scripts/SC_DoorScript.cs
+++ Nightmare_Descent_Into_Darkness/Assets/Scripts/SC_DoorScript.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class SC_DoorScript : MonoBehaviour
@@ -5,22 +6,22 @@ public class SC_DoorScript : MonoBehaviour
     public AnimationCurve openSpeedCurve = new AnimationCurve(new Keyframe[] { new Keyframe(0, 1, 0, 0), new Keyframe(0.8f, 1, 0, 0), new Keyframe(1, 0, 0, 0) });
     public float openSpeedMultiplier = 2.0f;
     public float doorOpenAngle = 90.0f;
-
+    public float closeDelay = 2.0f; // Delay before closing the door after everything exits its trigger zone
+    public GameObject doorInteractText;
     bool open = false;
     bool enter = false;
     bool hasEntered = false;
-    int aiInsideCount = 0; // Counter to keep track of the number of AI inside
+    int entityInsideCount = 0; // Counter to keep track of the number of entities (AI or player) inside the trigger zone
 
     float defaultRotationAngle;
     float currentRotationAngle;
     float openTime = 0;
-
-    public float closeDelay = 2.0f;
-
+    bool isPlayer=false;
     private AudioSource audioSource;
 
     void Start()
     {
+        doorInteractText.gameObject.SetActive(false);
         defaultRotationAngle = transform.localEulerAngles.y;
         currentRotationAngle = transform.localEulerAngles.y;
         audioSource = GetComponent<AudioSource>();
@@ -33,63 +34,90 @@ public class SC_DoorScript : MonoBehaviour
         {
             openTime += Time.deltaTime * openSpeedMultiplier * openSpeedCurve.Evaluate(openTime);
         }
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Mathf.LerpAngle(currentRotationAngle, defaultRotationAngle + (open ? doorOpenAngle : 0), openTime), transform.localEulerAngles.z);
+        transform.localEulerAngles = new Vector3(
+            transform.localEulerAngles.x,
+            Mathf.LerpAngle(currentRotationAngle, defaultRotationAngle + (open ? doorOpenAngle : 0), openTime),
+            transform.localEulerAngles.z
+        );
 
-        if (enter)
+     
+        if (enter &&!isPlayer)
         {
-            open = !open;
-            currentRotationAngle = transform.localEulerAngles.y;
-            openTime = 0;
-
-            if (!hasEntered)
-            {
-                hasEntered = true;
-                Invoke("CloseDoor", closeDelay);
-            }
+            OpenDoor();
         }
+        else if (enter && isPlayer)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                OpenDoor();
+                audioSource.PlayOneShot(audioSource.clip);
+
+            }
+
+        }
+       
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("AI"))
+        if (other.CompareTag("AI") )
         {
-            aiInsideCount++; // Increment the counter
             enter = true;
-
-            // Calculate the relative direction of the AI to the door
-            Vector3 doorForward = transform.forward;
-            Vector3 aiToDoor = other.transform.position - transform.position;
-            float dotProduct = Vector3.Dot(doorForward, aiToDoor);
-
-            // Apply opposite rotation based on the AI's position relative to the door
-            if (dotProduct > 0) // AI is in front of the door
-            {
-                doorOpenAngle = -Mathf.Abs(doorOpenAngle); // Open inward
-            }
-            else // AI is behind the door
-            {
-                doorOpenAngle = Mathf.Abs(doorOpenAngle); // Open outward
-            }
+            isPlayer = false;
+            HandleDoorForward(other);
             audioSource.PlayOneShot(audioSource.clip);
+
         }
+        if (other.CompareTag("Player"))
+        {
+            enter = true;
+            isPlayer = true;
+            doorInteractText.gameObject.SetActive(true);
+            HandleDoorForward(other);
+
+        }
+    }
+
+    private void HandleDoorForward(Collider other)
+    {
+        entityInsideCount++; // Increment the counter
+
+        Vector3 doorForward = transform.forward;
+        Vector3 entityToDoor = other.transform.position - transform.position;
+        float dotProduct = Vector3.Dot(doorForward, entityToDoor);
+
+        if (dotProduct > 0)
+        {
+            doorOpenAngle = -Mathf.Abs(doorOpenAngle);
+        }
+        else
+        {
+            doorOpenAngle = Mathf.Abs(doorOpenAngle);
+        }
+
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("AI"))
+        if (other.CompareTag("AI")||other.CompareTag("Player")) 
         {
-            aiInsideCount--; // Decrement the counter
-            if (aiInsideCount <= 0)
+            doorInteractText.gameObject.SetActive(false);
+            entityInsideCount--; // Decrement the counter
+            if (entityInsideCount <= 0)
             {
                 enter = false;
-                aiInsideCount = 0; // Reset the counter
+                isPlayer = false;
+                entityInsideCount = 0; // Reset the counter
+                CloseDoor();
             }
         }
+
+        
     }
 
     void CloseDoor()
     {
-        if (aiInsideCount <= 0) // Check if there are no AI inside before closing
+        if (entityInsideCount <= 0)
         {
             open = false;
             currentRotationAngle = transform.localEulerAngles.y;
@@ -97,4 +125,31 @@ public class SC_DoorScript : MonoBehaviour
             hasEntered = false;
         }
     }
+
+    void OpenDoor()
+    {
+        open = !open;
+        currentRotationAngle = transform.localEulerAngles.y;
+        openTime = 0;
+
+        if (!hasEntered)
+        {
+            hasEntered = true;
+            Invoke("CloseDoor", closeDelay);
+        }
+    }
 }
+
+
+
+
+
+
+   
+
+
+ 
+
+
+    
+
