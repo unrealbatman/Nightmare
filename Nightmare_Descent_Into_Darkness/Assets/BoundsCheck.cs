085d4056de7eacdebf8bb394315657e958297aff
Rithviik Srinivasan <rithu6876@gmail.com>
2023-12-05 00:59:05 -0500

    Added out of bounds check for main level

--- Nightmare_Descent_Into_Darkness/Assets/BoundsCheck.cs
+++ Nightmare_Descent_Into_Darkness/Assets/BoundsCheck.cs
@@ -1,19 +1,28 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoundsCheck : MonoBehaviour
 {
-
-
+    public TextMeshProUGUI outOfBoundsText;
  
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log(other.gameObject.tag);
-        if (other.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            Debug.Log("OUTOFBOUNDS");
+            outOfBoundsText.gameObject.SetActive(true);
         }
-       
     }
-    
-  
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            outOfBoundsText.gameObject.SetActive(false);
+        }
+    }
+
+
+
+
 }
