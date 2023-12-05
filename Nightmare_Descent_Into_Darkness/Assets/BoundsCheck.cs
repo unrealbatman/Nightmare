e980f0a681a63bbcaea30aa9a81345e4ce4c1235
Rithviik Srinivasan <rithu6876@gmail.com>
2023-12-05 12:31:58 -0500

    UI Work in progress

--- Nightmare_Descent_Into_Darkness/Assets/BoundsCheck.cs
+++ Nightmare_Descent_Into_Darkness/Assets/BoundsCheck.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BoundsCheck : MonoBehaviour
 {
-    public TextMeshProUGUI outOfBoundsText;
+    public TextMeshProUGUI   outOfBoundsText;
  
     private void OnTriggerEnter(Collider other)
     {
