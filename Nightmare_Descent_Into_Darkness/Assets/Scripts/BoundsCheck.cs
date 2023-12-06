9c64a4acc45121349738d5b6cfe5ba9094c25352
Rithviik Srinivasan <rithu6876@gmail.com>
2023-12-06 13:58:26 -0500

    UI + Cutscene work in progress

---
+++ Nightmare_Descent_Into_Darkness/Assets/Scripts/BoundsCheck.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+    public TextMeshProUGUI   outOfBoundsText;
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            outOfBoundsText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            outOfBoundsText.gameObject.SetActive(false);
+        }
+    }
+
+
+
+
+}
