59849e720429bfcf825a2a2a3ee1bd3c9d2ff9cc
Alexis Jin <xj606@nyu.edu>
2023-12-09 18:20:46 -0500

    "where is the key" text for level 1

--- Nightmare_Descent_Into_Darkness/Assets/Scripts/EnemyBehavior.cs
+++ Nightmare_Descent_Into_Darkness/Assets/Scripts/EnemyBehavior.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class EnemyBehavior : MonoBehaviour
 {
@@ -7,8 +9,11 @@ public class EnemyBehavior : MonoBehaviour
     public Animator anim;
     public AudioSource wraith;
     public GameObject key;
+    public TextMeshProUGUI whereIsKeyText;
+    public KeyPickUp keyPickUp;
 
     public float detectionRange = 2f;
+    public float whereIsKeyTextRange = 5f;
 
     void Update()
     {
@@ -16,11 +21,24 @@ public class EnemyBehavior : MonoBehaviour
             return;
 
         float distance = Vector3.Distance(player.transform.position, enemy.transform.position);
+        if(distance <= whereIsKeyTextRange && distance > detectionRange)
+        {
+            whereIsKeyText.enabled = true;
+        }
+        else
+        {
+            whereIsKeyText.enabled = false;
+        }
         if(distance <= detectionRange)
         {
+            whereIsKeyText.enabled = false;
             anim.SetTrigger("attack");
             wraith.PlayOneShot(wraith.clip);
             key.SetActive(true);
+        }
+        if(keyPickUp.key.enabled == false)
+        {
+            whereIsKeyText.enabled = false;
         }
     }
 }
