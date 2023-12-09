f0d68cd0f08170a40c82a3fc2282605764d08eeb
Alexis Jin <xj606@nyu.edu>
2023-12-09 18:41:05 -0500

    UI text adjustment

--- Nightmare_Descent_Into_Darkness/Assets/Scripts/EnemyBehavior.cs
+++ Nightmare_Descent_Into_Darkness/Assets/Scripts/EnemyBehavior.cs
@@ -9,11 +9,9 @@ public class EnemyBehavior : MonoBehaviour
     public Animator anim;
     public AudioSource wraith;
     public GameObject key;
-    public TextMeshProUGUI whereIsKeyText;
     public KeyPickUp keyPickUp;
 
     public float detectionRange = 2f;
-    public float whereIsKeyTextRange = 5f;
 
     void Update()
     {
@@ -21,24 +19,11 @@ public class EnemyBehavior : MonoBehaviour
             return;
 
         float distance = Vector3.Distance(player.transform.position, enemy.transform.position);
-        if(distance <= whereIsKeyTextRange && distance > detectionRange)
-        {
-            whereIsKeyText.enabled = true;
-        }
-        else
-        {
-            whereIsKeyText.enabled = false;
-        }
         if(distance <= detectionRange)
         {
-            whereIsKeyText.enabled = false;
             anim.SetTrigger("attack");
             wraith.PlayOneShot(wraith.clip);
             key.SetActive(true);
-        }
-        if(keyPickUp.key.enabled == false)
-        {
-            whereIsKeyText.enabled = false;
         }
     }
 }
--- Nightmare_Descent_Into_Darkness/Assets/Scripts/KeyPickUp.cs
+++ Nightmare_Descent_Into_Darkness/Assets/Scripts/KeyPickUp.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
+using System.Collections;
 
 public class KeyPickUp : MonoBehaviour
 {
     public TextMeshProUGUI keyText;
+    public TextMeshProUGUI whereIsKeyText;
+    public TextMeshProUGUI exitDoorText;
     public MeshRenderer key;
 
     public delegate void KeyPickedUp();
     public static event KeyPickedUp OnKeyPickedUp;
 
     bool isInRange = false;
+
+    void Start()
+    {
+        whereIsKeyText.enabled = true;
+        StartCoroutine(DisableText());
+    }
+
+    IEnumerator DisableText()
+    {
+        yield return new WaitForSeconds(3f);
+        whereIsKeyText.enabled = false;
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -47,5 +62,13 @@ public class KeyPickUp : MonoBehaviour
         key.enabled = false;
         OnKeyPickedUp?.Invoke();
         key.gameObject.SetActive(false);
+        exitDoorText.enabled = true;
+        StartCoroutine(DisableExitDoorText());
+    }
+
+    IEnumerator DisableExitDoorText()
+    {
+        yield return new WaitForSeconds(10f);
+        exitDoorText.enabled = false;
     }
 }
