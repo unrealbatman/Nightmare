941c283d6846179a00f8b0af297467dfba47708f
Alexis Jin <xj606@nyu.edu>
2023-12-09 18:11:15 -0500

    Revert "level 1 UI instruction"
    This reverts commit 54e10ea007e486183322a07aa6148e126a94a080.

--- Nightmare_Descent_Into_Darkness/Assets/Scripts/EnemyBehavior.cs
+++ Nightmare_Descent_Into_Darkness/Assets/Scripts/EnemyBehavior.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 
 public class EnemyBehavior : MonoBehaviour
 {
@@ -9,10 +7,8 @@ public class EnemyBehavior : MonoBehaviour
     public Animator anim;
     public AudioSource wraith;
     public GameObject key;
-    public TextMeshProUGUI findKeyText;
 
     public float detectionRange = 2f;
-    public float findKeyTextRange = 5f;
 
     void Update()
     {
@@ -20,24 +16,11 @@ public class EnemyBehavior : MonoBehaviour
             return;
 
         float distance = Vector3.Distance(player.transform.position, enemy.transform.position);
-        if(distance <= findKeyTextRange && distance > detectionRange)
-        {
-            findKeyText.enabled = true;
-        }
-        else
-        {
-            findKeyText.enabled = false;
-        }
         if(distance <= detectionRange)
         {
-            findKeyText.enabled = false;
             anim.SetTrigger("attack");
             wraith.PlayOneShot(wraith.clip);
             key.SetActive(true);
-        }
-        if(PlayerPrefs.GetInt("Level1key") == 1)
-        {
-            findKeyText.enabled = false;
         }
     }
 }
