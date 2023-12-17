467448e4946df22c5cdd9b96952431bb79ead74a
unknown <fz2244@nyu.edu>
2023-12-16 22:47:49 -0500

    fix heartbeat& add death

---
+++ Nightmare_Descent_Into_Darkness/Assets/DeathSound.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSound : MonoBehaviour
+{
+
+    public AudioSource sound;
+    private void OnEnable()
+    {
+        AIController.OnPlayerDie += play;
+    }
+
+    private void OnDisable()
+    {
+        AIController.OnPlayerDie -= play;
+    }
+    private void play()
+    {
+        sound.Play();
+    }
+}
